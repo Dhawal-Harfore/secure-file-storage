c2e5d5b3bc67bb6b245c2078a918f5d2d03c1cfc
Dhawal-Harfore <305175429+Dhawal-Harfore@users.noreply.github.com>
2026-09-01 18:21:59 +0530

    Updated backend

--- src/main/java/com/dhawal/secure_file_storage/controller/TestController.java
+++ src/main/java/com/dhawal/secure_file_storage/controller/TestController.java
@@ -2,9 +2,17 @@ package com.dhawal.secure_file_storage.controller;
 
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+import com.dhawal.secure_file_storage.dto.UserRequest;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 @RestController
 public class TestController {
+
+    @PostMapping("/api/users")
+    public UserRequest createUser(@RequestBody UserRequest user) {
+        return user;
+    }
 
     @PostMapping("/api/test")
     public String test() {
